bd24d9546c721834bce8a9cabe4fb9a57621f105
Nick Jaeger <jaegernh@uwec.edu>
2015-03-13 11:09:36 -0500

    add initial code and dependencies
    Add python script, templates, styles, js, and other dependencies.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dcc-py-blogger
+devCodeCamp Python Blogger - generates clean a clean HTML page of blog entires from JSON
+
+
+trying something
---
+++ make.py
@@ -0,0 +1,34 @@
+import sys
+import json
+import os
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    print "generating blog..."
+
+    #read in template information
+    index = open(os.path.join(path, 'index.dcc'), 'r').read()
+    entryStyle = open(os.path.join(path, 'entry_style.dcc'), 'r').read()
+
+    #read in blog entries from json file
+    with open(os.path.join(path, 'entries.json'),'r') as data_file:    
+        entries = json.load(data_file)
+
+    #style blog entries
+    styledEntries=""
+    for entry in entries["entries"]:
+        styledEntries=styledEntries+'\n'+entryStyle.format(entry["date"],entry["title"],entry["text"])
+
+    #add format wrapping
+    index = index.format(styledEntries)
+
+    #write all entries into index
+    index_file = open(os.path.join(path, 'index.html'), "w")
+    index_file.write(index)
+    index_file.close()
+
+except Exception,e:
+    print str(sys.exc_traceback.tb_lineno) +":"+str(e)
+
+print "done."
