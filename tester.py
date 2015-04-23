16daa20780ad532e79b2d4e4575ac6a1d420b9a3
Spencer <sritzow_h@dev.bse.edu>
2015-04-23 18:06:17 -0500

    blog post

--- tester.py
+++ tester.py
@@ -3,8 +3,4 @@ import pymongo
 conn = pymongo.MongoClient()
 db = conn.tweets
 
-for c in db.collection_names():
-	print c
-items = db.tweets.find()
-for item in items:
-	print item
+db.tweets.dropDatabase()
