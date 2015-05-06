b581768550d52765dbfbf32c2598c1b52b765c2c
unknown <Sritzow_H@Dev03.bsecode.inc>
2015-05-06 10:35:47 -0500

    pager

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -1,17 +1,27 @@
 $(document).ready(getposts);
+
+var posts = [];
+
+function pager(page) {
+	var text = "";
+	for (i = (page - 1 * 5); i < posts.length; i++) {
+		text += posts[i];
+	}	
+	$('#blog').html(text);
+}
+
 function getposts(){
 	console.log("loading");
 	$.get("entries.json", function( data ) {
 		var posts = "";
 		var data = data['entries'];
 		$.each(data, function(key, value) {
-			console.log(key + ' -- ' + value);
 			var date = value['date'];
 			var title = value['title'];
 			var text = value['text'];
-			posts += "<div id='key' class='col-sm-12 blogpost'><small> <p class='muted' style='float:right;'>" + date + "</p></small><h5>" + title + "</h5><p>" + text + "</p><hr/></div>";
+			posts.push("<div id='key' class='col-sm-12 blogpost'><small> <p class='muted' style='float:right;'>" + date + "</p></small><h5>" + title + "</h5><p>" + text + "</p><hr/></div>");
 		});
 		
-		$('#blog').html(posts);
+		pager(1);
 	});
 }
