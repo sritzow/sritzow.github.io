0ee361fe71dd2c4853dc29c06fbbbe73b635354a
unknown <Sritzow_H@Dev03.bsecode.inc>
2015-05-06 10:29:51 -0500

    no message

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -9,7 +9,7 @@ function getposts(){
 			var date = value['date'];
 			var title = value['title'];
 			var text = value['text'];
-			posts += "<div id='key' class='col-sm-12 blogpost'><small> <p class='muted' style='float:right;'>" + post + "</p></small><h5>" + title + "</h5><p>" + text + "</p><hr/></div>";
+			posts += "<div id='key' class='col-sm-12 blogpost'><small> <p class='muted' style='float:right;'>" + date + "</p></small><h5>" + title + "</h5><p>" + text + "</p><hr/></div>";
 		});
 		
 		$('#blog').html(posts);
