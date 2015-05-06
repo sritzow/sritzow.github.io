0ddf757cd9965764b81a55d8a86636e32343e890
unknown <Sritzow_H@Dev03.bsecode.inc>
2015-05-06 10:16:42 -0500

    test

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -2,12 +2,11 @@ $(document).ready(getposts);
 function getposts(){
 	console.log("loading");
 	$.get("entries.json", function( data ) {
-		
 		var posts = [];
+		var data = data['entries'];
 		$.each(data, function(key, value) {
 			console.log(key + ' -- ' + value);
 			//posts.push("<div id='key' class='col-sm-12 blogpost'><small> <p class='muted' style='float:right;'>" + data[key] + "</p>")
-			
 		});
 	});
 }
