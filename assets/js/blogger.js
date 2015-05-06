88d00847686af62d20701f9f53cabe628a440ffb
unknown <Sritzow_H@Dev03.bsecode.inc>
2015-05-06 10:18:27 -0500

    test

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -6,6 +6,10 @@ function getposts(){
 		var data = data['entries'];
 		$.each(data, function(key, value) {
 			console.log(key + ' -- ' + value);
+			var date = value['date'];
+			var title = value['title'];
+			var post = value['text'];
+			console.log(date + ' - ' + title + ' - ' + post);
 			//posts.push("<div id='key' class='col-sm-12 blogpost'><small> <p class='muted' style='float:right;'>" + data[key] + "</p>")
 		});
 	});
