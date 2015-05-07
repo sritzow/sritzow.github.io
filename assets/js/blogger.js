a62cfdac66f34d80e500646c962cda9781d869f5
unknown <Sritzow_H@Dev03.bsecode.inc>
2015-05-07 09:07:45 -0500

    test highlighter

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -35,12 +35,38 @@ function getSearchPosts() {
 	return posts;
 }
 
+function preg_quote( str ) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: booeyOH
+    // +   improved by: Ates Goral (http://magnetiq.com)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
+    // *     example 1: preg_quote("$40");
+    // *     returns 1: '\$40'
+    // *     example 2: preg_quote("*RRRING* Hello?");
+    // *     returns 2: '\*RRRING\* Hello\?'
+    // *     example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
+    // *     returns 3: '\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:'
+
+    return (str+'').replace(/([\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:])/g, "\\$1");
+}
+
+function highlight( data, search )
+{
+    return data.replace( new RegExp( "(" + preg_quote( search ) + ")" , 'gi' ), "<b>$1</b>" );
+}
+
 function pagePosts(searchPosts, page, scroll) {
 	var text = "";
 	for (i = (page - 1) * 5; i < (page - 1) * 5 + 5; i++) {
 		if (searchPosts != null) {
-			if (searchPosts.length > i)
-				text += "<div id='post" + i + "' class='col-sm-12 blogpost'><small> <p class='muted' style='float:right;'>" + searchPosts[i]['date'] + "</p></small><h5>" + searchPosts[i]['title'] + "</h5><p>" + searchPosts[i]['text'] + "</p><hr/></div>";
+			if (searchPosts.length > i) {
+				var postText = searchPosts[i]['text'];
+				if ($('#search-second').val() != null && $('#search-second').val() != '') {
+					postText = highlight( postText, $('#search-second').val());
+				}
+				text += "<div id='post" + i + "' class='col-sm-12 blogpost'><small> <p class='muted' style='float:right;'>" + searchPosts[i]['date'] + "</p></small><h5>" + searchPosts[i]['title'] + "</h5><p>" + postText + "</p><hr/></div>";
+			}
 		}
 	}
 	
