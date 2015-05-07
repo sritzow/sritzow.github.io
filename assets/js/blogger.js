d3a1d8b292743cf12dd86c130c13c340d8e5c89d
unknown <Sritzow_H@Dev03.bsecode.inc>
2015-05-07 09:45:10 -0500

    removed useless logs and depreciated function

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -83,7 +83,7 @@ function pagePosts(searchPosts, page, scroll) {
 		}
 		
 		if (i + 1 == page) {
-			under += ' <span onclick = "pager(' + (i + 1) + ', true)" style = "cursor: pointer; text-decoration:underline;">' + (i + 1) + '</span>'
+			under += ' <span onclick = "pagePosts(getSearchPosts(), ' + (i + 1) + ', true)" style = "cursor: pointer; text-decoration:underline;">' + (i + 1) + '</span>'
 		} else {
 			under += ' <span style = "cursor: pointer;" onclick = "pagePosts(getSearchPosts(), ' + (i + 1) + ', true)">' + (i + 1) + '</span>';
 		}
@@ -101,57 +101,16 @@ function pagePosts(searchPosts, page, scroll) {
 		$('#blogheader')[0].scrollIntoView(true);
 }
 
-function pager(page, scroll) {
-	var text = "";
-	
-	var under = '<p class = "lead" style = "text-align:center">';
-	for (i = (page - 1) * 5; i < (page - 1) * 5 + 5; i++) {
-		if (posts.length > i) {
-			console.log(i + ' - ' + posts[i]);
-			text += posts[i];
-		}
-	}	
-	
-	for (i = 0; i < Math.ceil(posts.length / 5); i++) {
-		if (i == 0 && page != 1) {
-			under += ' <span style = "cursor: pointer;" onclick = "pagePosts(posts, ' + (page - 1) + ', true)">Previous</span>';
-		}
-		
-		if (i + 1 == page) {
-			under += ' <span onclick = "pager(' + (i + 1) + ', true)" style = "cursor: pointer; text-decoration:underline;">' + (i + 1) + '</span>'
-		} else {
-			under += ' <span style = "cursor: pointer;" onclick = "pagePosts(posts, ' + (i + 1) + ', true)">' + (i + 1) + '</span>';
-		}
-		
-		if (i == Math.ceil(posts.length / 5) - 1 && page != Math.ceil(posts.length / 5)) {
-			under += ' <span style = "cursor: pointer;" onclick = "pager(' + (page + 1) + ', true)">Next</span>';
-		}
-	}
-	
-	under += '</p>';
-	
-	text += under;
-	$('#blog').fadeOut('slow', function() {
-		$('#blog').html(text).hide().fadeIn('slow');
-	});
-	if (scroll)
-		$('#blogheader')[0].scrollIntoView(true);
-}
-
 function getposts(){
-	console.log("loading");
 	$.get("entries.json", function( data ) {
-		console.log(data);
 		var data = data['entries'].reverse();
 		$.each(data, function(key, value) {
-			console.log(value);
 			var date = value['date'];
 			var title = value['title'];
 			var text = value['text'];
 			//posts.push("<div id='post" + key + "' class='col-sm-12 blogpost'><small> <p class='muted' style='float:right;'>" + date + "</p></small><h5>" + title + "</h5><p>" + text + "</p><hr/></div>");
 			posts.push(value);
 		});
-		//pager(1, false);
 		pagePosts(posts, 1, false);
 	});
 }
